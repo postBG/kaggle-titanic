280caf78866f18e3dca4e2798d5fd918df4682df
unknown <profile2697@gmail.com>
2017-09-22 13:06:37 +0900

    add decision tree (0.71770)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 from data import TitanicData
-from models import SimpleGenderModel
+from models import SimpleGenderModel, MyDecisionTreeClassifier
 from printer import DataFramePrinter
 
 FLAGS = tf.app.flags.FLAGS
@@ -11,15 +11,18 @@ tf.app.flags.DEFINE_string('preprocess_dir', 'preprocess',
                            """Directory path that saves preprocessed data as csv""")
 tf.app.flags.DEFINE_string('simple_output', 'output_simple.csv',
                            """Directory path that saves simple gender model output as csv""")
+tf.app.flags.DEFINE_string('decision_output', 'output_decision.csv',
+                           """Directory path that saves simple gender model output as csv""")
 
 
 def main(argv=None):
     titanic = TitanicData(FLAGS.data_dir, FLAGS.preprocess_dir)
     titanic.preprocess_and_save()
-
-    simple_model = SimpleGenderModel()
     printer = DataFramePrinter()
-    printer.to_csv(simple_model.predict(titanic.processed_test), FLAGS.simple_output)
+
+    model = MyDecisionTreeClassifier(max_depth=5)
+    model.fit(titanic.processed_train)
+    printer.to_csv(model.predict(titanic.processed_test), FLAGS.decision_output)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,6 +1,37 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+
 class Model:
     def predict(self, data):
         raise NotImplementedError
+
+
+def _print_stats(predicted, labels):
+    correct = sum([p == l for p, l in zip(predicted, labels)])
+    print('model accuracy: {:2}'.format(correct / len(labels)))
+
+
+class SklearnModel(Model):
+    def fit(self, train_data):
+        features, labels = self._seperate_features_and_labels(train_data)
+        self.model.fit(features, labels)
+
+        # not a validation. it is just information.
+        _print_stats(self.model.predict(features), labels)
+
+    def predict(self, data):
+        ids = data['PassengerId']
+        labels = self.model.predict(data)
+
+        return pd.DataFrame(data=[(passenger_id, survive,) for passenger_id, survive in zip(ids, labels)],
+                            columns=['PassengerId', 'Survived'])
+
+    def _seperate_features_and_labels(self, train_data):
+        train_labels = train_data['Survived']
+        train_features = train_data.drop('Survived', axis=1)
+
+        return train_features, train_labels
 
 
 class SimpleGenderModel(Model):
@@ -10,3 +41,8 @@ class SimpleGenderModel(Model):
         })
 
         return answer
+
+
+class MyDecisionTreeClassifier(SklearnModel):
+    def __init__(self, *args, **kwargs):
+        self.model = DecisionTreeClassifier(*args, **kwargs)
