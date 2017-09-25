0dc67522122d9fc03bfa6c6477c10dc37dfce881
unknown <profile2697@gmail.com>
2017-09-26 01:57:42 +0900

    add some layers module component

--- nn/model.py
+++ nn/model.py
@@ -1,14 +1,15 @@
 import tensorflow as tf
 
-from models import Model
+from .layers import fully_connected, dropout
 
 
-class SimpleFeedFoward(Model):
+class SimpleFeedForwardNetwork:
     def __init__(self, config, inputs):
         self.inputs = inputs
 
         self.drop_rate = config.drop_rate
+        self.bottleneck_n_units = config.bottleneck_n_units
         self.config = config
 
     def build_graph(self, inputs):
-        pass
+        self.bottleneck_layer = fully_connected(inputs, self.bottleneck_n_units, name="bottleneck")
